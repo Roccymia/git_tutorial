2c5163497f06b70765806495154cea025307693b
Dingdong <mahyuddin.b@ku.th>
2023-01-25 15:39:04 +0700

    Midterm Exam Q2

---
+++ q2/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/din/git_tutorial/q2/src
+# Build directory: /home/din/git_tutorial/q2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ q2/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/din/git_tutorial/q2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/din/tutorial_ws/devel;/home/din/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/din/tutorial_ws/devel;/home/din/catkin_ws/devel;/opt/ros/noetic' != '' else []
